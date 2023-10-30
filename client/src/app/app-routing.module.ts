3e661c6af52ab8ca1595773d90a381d709b9faa2
juldyzmurat <105715486+juldyzmurat@users.noreply.github.com>
2023-10-29 21:11:04 -0400

    kitty is showing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { UsersListComponent } from './users-list/users-list.component';
 import { AddUserComponent } from './add-user/add-user.component'; // <-- add this line
 import { EditUserComponent } from './edit-user/edit-user.component'; // <-- add this line
+import { TaskpageComponent } from './taskpage/taskpage.component';
+
  
 const routes: Routes = [
  { path: '', redirectTo: 'users', pathMatch: 'full' },
  { path: 'users', component: UsersListComponent },
  { path: 'users/new', component: AddUserComponent }, // <-- add this line
- { path: 'users/edit/:id', component: EditUserComponent }// <-- add this line
+ { path: 'users/edit/:id', component: EditUserComponent },// <-- add this line
+ { path: 'taskpage', component: TaskpageComponent }
 ];
  
 @NgModule({
