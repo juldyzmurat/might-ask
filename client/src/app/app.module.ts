027cd0a8f018ccc3b8de53b11341e564fc8b1107
Joshua Shew <joshua.t.shew@gmail.com>
2023-10-30 23:31:02 -0400

    created login component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TaskpageComponent } from './taskpage/taskpage.component';
 import { ListviewComponent } from './listview/listview.component';
 import { BoardviewComponent } from './boardview/boardview.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     EditUserComponent,
     TaskpageComponent,
     ListviewComponent,
-    BoardviewComponent
+    BoardviewComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
