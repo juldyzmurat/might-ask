8325dce9585d4e61b6e3c815a8007bbde9db520b
Jimmy Gallegos <130590885+HaruJGG@users.noreply.github.com>
2023-11-10 17:58:29 -0500

    Co-authored-by: juldyzmurat <juldyzmurat@users.noreply.github.com> Co-authored-by: adighosh18 <adighosh18@users.noreply.github.com>
    React Tabs completed

--- client/mightask-ui/src/App.js
+++ client/mightask-ui/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDom from 'react-dom';
+//import gif from './20cat.gif';
 import './App.css';
+import Tabs from "./Components/TabComponent/Tabs";
+import listscroller from "./Components/TaskVisComponents/listscroller";
+
+// For the cat  <img src={gif} className="App-logo" alt="gif" />
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header"> 
       </header>
+
+      <Tabs />
+      <listscroller />
     </div>
   );
 }
---
+++ client/mightask-ui/src/Components/AllTabs/ListView.js
@@ -0,0 +1,10 @@
+const FirstTab = () => {
+    return (
+      <div className="FirstTab">
+        <p>First Tab!! Hurray!! List View</p>
+        {/* First tab content will go here */}
+      </div>
+    );
+  };
+  export default FirstTab;
+  
