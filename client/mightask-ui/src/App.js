00f7cfc0913d456c5c717718e343fc3f2f495382
Jimmy Gallegos <130590885+HaruJGG@users.noreply.github.com>
2023-11-12 19:21:59 -0500

    Navbar

--- client/mightask-ui/src/App.js
+++ client/mightask-ui/src/App.js
@@ -3,6 +3,16 @@ import gif from './20cat.gif';
 import './App.css';
 import Tabs from "./Components/TabComponent/Tabs";
 import CircleButton from "./Components/TaskVisComponents/accounticon"; 
+import TaskView from './Components/Pages/TasksViewPage';
+import Dashboard from "./Components/Pages/DashboardPage";
+import Navbar from './Components/NavBar/';
+import { BrowserRouter as Router, Routes, Route }
+    from 'react-router-dom';
+import Home from './Components/Pages/';
+import About from './Components/Pages/about';
+import Blogs from './Components/Pages/blogs';
+import SignUp from './Components/Pages/signup';
+import Contact from './Components/Pages/contact';
 
 
 // For the cat  <img src={gif} className="App-logo" alt="gif" />
@@ -18,6 +28,16 @@ function App() {
         <CircleButton />
         <img src={gif} className="App-logo" alt="gif" />
       </header>
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/blogs' element={<Blogs />} />
+                <Route path='/sign-up' element={<SignUp />} />
+            </Routes>
+        </Router>
 
       <Tabs />
     </div>
---
+++ client/mightask-ui/src/Components/Pages/TasksViewPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import Tabs from "../TabComponent/Tabs";
+
+
+// For the cat  <img src={gif} className="App-logo" alt="gif" />
+
+
+function TaskView() {
+  return (
+    <div className="TaskView">
+      <Tabs />
+    </div>
+  );
+}
+
+export default TaskView;
