9cd91bdfba3b18c1f1391319536bcab98758405a
Jimmy Gallegos <130590885+HaruJGG@users.noreply.github.com>
2023-11-13 13:16:53 -0500

    Moved sing in to landing page

--- client/mightask-ui/src/Components/Pages/index.js
+++ client/mightask-ui/src/Components/Pages/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Google from "../Login/LoginAPI";
+import { GoogleOAuthProvider } from '@react-oauth/google'; 
+import gif from '../../80cat.gif';
  
 const Home = () => {
     return (
         <div>
-            <h1>Welcome to migTASK</h1>
+            <img src={gif} className="App-logo" alt="gif" />
+            <GoogleOAuthProvider clientId="599824373793-o5aoosfc8ndecst0jq232s9qjqdhmr83.apps.googleusercontent.com">
+                <Google />
+            </GoogleOAuthProvider>
         </div>
     );
 };
