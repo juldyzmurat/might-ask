db8080bd4190b0262bb337804bc1a9a659e0df46
Joshua Shew <joshua.t.shew@gmail.com>
2023-11-14 00:00:38 -0500

    Added a successful load test to App.test.js

--- client/mightask-ui/src/App.test.js
+++ client/mightask-ui/src/App.test.js
@@ -1,7 +1,13 @@
 import renderer from 'react-test-renderer';
+import render from '@testing-library/react';
 import App from './App';
 
 it('renders correctly', () => {
   const tree = renderer.create(<App />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('has a routes element', () => {
+  const view = render(<App />);
+  expect(view).toBeTruthy();
 });
