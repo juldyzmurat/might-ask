ec2cdd296a8a1fb4b099b132992a8cb2a791cd4b
Joshua Shew <joshua.t.shew@gmail.com>
2023-11-20 07:46:10 -0500

    Simplified App.js test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,7 @@
-import render from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 it('renders', () => {
-  const view = render(<App />);
-  expect(view).toBeTruthy();
+  render(<App />);
+  expect(screen).toBeTruthy();
 });
