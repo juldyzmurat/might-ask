6f0286fe4b9159766dd616d1997e8fae71086601
Joshua Shew <joshua.t.shew@gmail.com>
2023-11-27 22:55:49 -0500

    Changed Kanban to display new kanban

--- client/src/Components/Kanban/Kanban.js
+++ client/src/Components/Kanban/Kanban.js
@@ -1,26 +1,7 @@
-import Stack from "@mui/material/Stack";
-import TaskCard from "../Tasks/TaskCard";
+import TaskList from "./TaskList";
 
 const Kanban = ({ categories }) => {
-  const tasks = [
-    { name: "Presentation", dueDate: "2022-11-30", category: "School" },
-    { name: "Meeting", dueDate: "2022-11-27", category: "Work" },
-    {
-      name: "a task with a much longer name for testing purposes",
-      dueDate: "2024-01-01",
-      category: "Personal",
-    },
-  ];
-
-  return (
-    <div className="Kanban">
-      <Stack spacing={2}>
-        {tasks.map((task, index) => (
-          <TaskCard key={index} task={task} categories={categories} />
-        ))}
-      </Stack>
-    </div>
-  );
+  return <TaskList />;
 };
 
 export default Kanban;
