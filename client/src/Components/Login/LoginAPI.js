a4a1ac665d337c6c47bd2910cff84937d4378f11
skhadkikar <sxk1654@case.edu>
2023-11-28 19:47:21 -0500

    Inserts user to DB on login if user is new

--- client/src/Components/Login/LoginAPI.js
+++ client/src/Components/Login/LoginAPI.js
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { GoogleLogin } from "react-google-login";
+import React, { useState, useEffect } from "react";
 
 const clientID =
   "613216441734-0c8nmpfakholp4jm5v5jp14occlu232i.apps.googleusercontent.com";
@@ -13,6 +14,56 @@ function LoginButton() {
     console.log("LOGIN SUCCESS Current user: ", res);
     GoogleData = res;
     navigate("/TasksViewPage");
+
+    // check user with database
+    var currentUser = false;
+    const fetchCurrentUser = async() => {
+        try {
+            const request = "http://localhost:5200/users/".concat(GoogleData.profileObj.email);
+            const response = await fetch(request);
+            if (!response.ok) {
+                throw new Error("Failed to fetch data");
+            }
+            const jsonData = await response.json();
+            currentUser = jsonData;
+        } catch (error) {
+            console.error("Error fetching data: ", error.message);
+            currentUser = false;
+        }
+    };
+    fetchCurrentUser();
+    if (!currentUser) {
+        const userFormData = {
+            firstname: GoogleData.profileObj.givenName,
+            lastname: GoogleData.profileObj.familyName,
+            email: GoogleData.profileObj.email,
+            access: "normal",
+            profile_path: "path/name",
+        };
+
+        const fetchNewUser = async() => {
+            try {
+                const request = "http://localhost:5200/users/";
+                const data = JSON.stringify(userFormData);
+                const response = await fetch(request, {
+                    method: "post",
+                    // mode: "cors",
+                    headers: {'Content-Type': 'application/json'},
+                    body: data,
+                });
+                // console.log(data);
+                console.log("fetch");
+                if (!response.ok) {
+                    throw new Error("Failed to post data");
+                }
+            } catch (error) {
+                console.error("Error fetching data: ", error.message);
+            }
+        };
+        fetchNewUser();
+    }
+
+    // create categories for user
   };
 
   const onFailure = (res) => {
--- client/src/Components/TaskVisComponents/popupaddtask.js
+++ client/src/Components/TaskVisComponents/popupaddtask.js
@@ -36,7 +36,7 @@ const TaskForm = ({ onClose }) => {
   const handleSelect = async (address) => {
     try {
       const results = await geocodeByAddress(address);
-      //const latLng = await getLatLng(results[0]);
+      // const latLng = await getLatLng(results[0]);
       setLocation(address);
       // You can also store the coordinates if needed: setCoordinates(latLng);
     } catch (error) {
@@ -78,7 +78,7 @@ const TaskForm = ({ onClose }) => {
             // console.log(data);
             console.log("fetch");
             if (!response.ok) {
-                throw new Error("Failed to fetch data");
+                throw new Error("Failed to post data");
             }
         } catch (error) {
             console.error("Error fetching data: ", error.message);
