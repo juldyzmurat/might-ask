1ae19fd5a7dd700caa3f79f7d01cf4e29c29fdeb
Joshua Shew <joshua.t.shew@gmail.com>
2023-11-29 18:59:31 -0500

    Linted all files

--- client/src/App.js
+++ client/src/App.js
@@ -14,14 +14,17 @@ function App() {
       <header className="App-header"></header>
       <Router>
         <Navbar />
-        <div classname="main-box"style={{
-  background: "#f1b6dc",
-  margin: "3.5rem auto 1.5rem",
-  padding: "2rem 1rem",
-  color: "#e8f0f2",
-  borderRadius: "2rem",
-  paddingTop: "5%"
-}}>
+        <div
+          classname="main-box"
+          style={{
+            background: "#f1b6dc",
+            margin: "3.5rem auto 1.5rem",
+            padding: "2rem 1rem",
+            color: "#e8f0f2",
+            borderRadius: "2rem",
+            paddingTop: "5%",
+          }}
+        >
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
