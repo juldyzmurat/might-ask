6de4814fc56cf700c2b1d7f1edf321adc6135d2b
Jimmy Gallegos <130590885+HaruJGG@users.noreply.github.com>
2023-11-30 02:50:01 -0500

    board with names

--- client/src/Components/TaskViews/BoardView/Board/Board.jsx
+++ client/src/Components/TaskViews/BoardView/Board/Board.jsx
@@ -1,30 +1,9 @@
-import React, { useEffect, useState } from "react";
-
+import React from "react";
 import "./Board.css";
-
 import { Droppable } from "react-beautiful-dnd";
-// ... (existing imports)
 
 export default function Board(props) {
-  const [show, setShow] = useState(false);
-
-  useEffect(() => {
-    document.addEventListener("keypress", (e) => {
-      if (e.code === "Enter") setShow(false);
-    });
-    return () => {
-      document.removeEventListener("keypress", (e) => {
-        if (e.code === "Enter") setShow(false);
-      });
-    };
-  });
-
-  // Define names for the three static boards
-  const boardNames = {
-    1: "Board One",
-    2: "Board Two",
-    3: "Board Three",
-  };
+  const { name } = props;
 
   // Check if the board's id is within the range [1, 3]
   if (props.id < 1 || props.id > 3) {
@@ -41,29 +20,11 @@ export default function Board(props) {
             {...provided.droppableProps}
           >
             <div className="board__top">
-              {show ? (
-                <div>
-                  <input
-                    className="title__input"
-                    type={"text"}
-                    defaultValue={boardNames[props.id]}
-                    onChange={(e) => {
-                      props.setName(e.target.value, props.id);
-                    }}
-                  />
-                </div>
-              ) : (
-                <div>
-                  <p
-                    onClick={() => {
-                      setShow(true);
-                    }}
-                    className="board__title"
-                  >
-                    {boardNames[props.id] || "Name of Board"}
-                  </p>
-                </div>
-              )}
+              <div>
+                <p className="board__title">
+                  {name || "Name of Board"}
+                </p>
+              </div>
             </div>
             {provided.placeholder}
           </div>
