153279b4fa145171f4852720a1ebe057f79bbe57
Joshua Shew <joshua.t.shew@gmail.com>
2023-11-29 21:47:39 -0500

    Reversed recent changes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-// import { Admin } from "react-admin";
-// import simpleRestProvider from "ra-data-simple-rest";
 
 import "./App.css";
 import TaskViews from "./Components/Pages/TaskViews";
@@ -11,37 +9,33 @@ import About from "./Components/Pages/about";
 import Schedule from "./Components/Pages/schedule";
 import Dashboard from "./Components/Pages/DashboardPage";
 
-// const dataProvider = simpleRestProvider("http://localhost:5200/");
-
 function App() {
   return (
-    // <Admin dataProvider={dataProvider}>
-      <div className="App">
-        <header className="App-header"></header>
-        <Router>
-          <Navbar />
-          <div
-            className="main-box"
-            style={{
-              background: "#f1b6dc",
-              margin: "3.5rem auto 1.5rem",
-              padding: "2rem 1rem",
-              color: "#e8f0f2",
-              borderRadius: "2rem",
-              paddingTop: "5%",
-            }}
-          >
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/task-views" element={<TaskViews />} />
-              <Route path="/DashboardPage" element={<Dashboard />} />
-              <Route path="/schedule" element={<Schedule />} />
-            </Routes>
-          </div>
-        </Router>
-      </div>
-    // </Admin>
+    <div className="App">
+      <header className="App-header"></header>
+      <Router>
+        <Navbar />
+        <div
+          className="main-box"
+          style={{
+            background: "#f1b6dc",
+            margin: "3.5rem auto 1.5rem",
+            padding: "2rem 1rem",
+            color: "#e8f0f2",
+            borderRadius: "2rem",
+            paddingTop: "5%",
+          }}
+        >
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/task-views" element={<TaskViews />} />
+            <Route path="/DashboardPage" element={<Dashboard />} />
+            <Route path="/schedule" element={<Schedule />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
 }
 
