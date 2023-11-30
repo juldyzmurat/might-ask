3d75406c0a8051e3336886f5c62cbb3dccef2814
Jimmy Gallegos <130590885+HaruJGG@users.noreply.github.com>
2023-11-30 00:56:57 -0500

    Update categories and visuals

--- client/src/Components/Dashboard/CategoryDashboard.js
+++ client/src/Components/Dashboard/CategoryDashboard.js
@@ -18,16 +18,6 @@ const CategoryDashboard = () => {
         }
         const taskData = await taskResponse.json();
         setData(taskData);
-
-        const categoryRequest = "http://localhost:5200/categories/".concat(
-          GoogleData.profileObj.email,
-        );
-        const categoryResponse = await fetch(categoryRequest);
-        if (!categoryResponse.ok) {
-          throw new Error("Failed to fetch category data");
-        }
-        const categoryJsonData = await categoryResponse.json();
-        setCategoryData(categoryJsonData);
       } catch (error) {
         console.error("Error fetching data:", error.message);
       }
@@ -43,22 +33,24 @@ const CategoryDashboard = () => {
     categoryCount[categoryId] = (categoryCount[categoryId] || 0) + 1;
   });
 
+  console.log("cc",categoryCount);
+
   const newJson = Object.entries(categoryCount).map(([categoryId, count]) => ({
     categoryId,
-    categoryName:
-      categoryData.find((categoryItem) => categoryItem._id === categoryId)
-        ?.name || "Unkown",
     count,
-    color: categoryData.find((categoryItem) => categoryItem._id === categoryId)
-      ?.color,
+    
   }));
 
-  const transformedJson = newJson.map((item) => ({
-    name: `Category ${item.categoryName}`,
-    count: item.count,
-    color: item.color,
-  }));
-  console.log(transformedJson);
+  
+    const pastelColors = ['#FFB6C1', '#FFD700', '#87CEEB', '#98FB98', '#FFA07A', '#DDA0DD'];
+  
+    const transformedJson = newJson.map((item, index) => ({
+      name: `Category ${item.categoryId}`,
+      count: item.count,
+      color: pastelColors[index % pastelColors.length],
+    }));
+
+  
 
   return (
     <div className="container">
@@ -66,14 +58,13 @@ const CategoryDashboard = () => {
         <PieChart
           width={500}
           height={500}
-          style={{ backgroundColor: "#f1b6dc" }}
+          style={{ backgroundColor: "#fffff" }}
         >
           <Pie
             data={transformedJson}
             dataKey="count"
             outerRadius={200}
-            fill="pink"
-            label
+            fill="black"
           >
             {transformedJson.map((entry, index) => (
               <Cell key={`cell-${index}`} fill={entry.color} />
